db934986c5a2c447d5430a9ccbee95ed12b3db03
Miguel Rivas <mangelrivasm@hotmail.com>
2019-05-30 22:56:42 -0500

    sdsds

--- Ayudantia/src/ayudantia/Clase2.java
+++ Ayudantia/src/ayudantia/Clase2.java
@@ -13,5 +13,6 @@ public class Clase2 {
     public static void main(String args[]){
         System.out.println("hola soy tu companer");
         System.out.println("segundo print");
+        System.out.println("asas");
     }
 }
